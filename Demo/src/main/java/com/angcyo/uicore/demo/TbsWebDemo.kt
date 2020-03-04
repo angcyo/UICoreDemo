8b2b86e1ed99cfc076fc5c5081082d76924bf3d1
angcyo <angcyo@126.com>
2020-03-04 20:51:27 +0800

    + tbs file chooser demo

--- Demo/src/main/java/com/angcyo/uicore/demo/TbsWebDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/TbsWebDemo.kt
@@ -4,7 +4,9 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import com.angcyo.base.dslAHelper
+import com.angcyo.base.dslFHelper
 import com.angcyo.component.hawkInstallAndRestore
+import com.angcyo.core.component.fileSelector
 import com.angcyo.dsladapter.renderItem
 import com.angcyo.tbs.open
 import com.angcyo.uicore.base.AppDslFragment
@@ -44,6 +46,30 @@ class TbsWebDemo : AppDslFragment() {
                         dslAHelper {
                             open {
                                 uri = Uri.parse(itemHolder.tv(R.id.edit_text3).string())
+                            }
+                        }
+                    }
+
+                    itemHolder.click(R.id.local_file_chooser) {
+                        dslAHelper {
+                            open {
+                                uri = Uri.parse("file:///android_asset/webpage/fileChooser.html")
+                            }
+                        }
+                    }
+
+                    itemHolder.click(R.id.local_file_play) {
+                        dslAHelper {
+                            open {
+                                uri = Uri.parse("file:///android_asset/webpage/fullscreenVideo.html")
+                            }
+                        }
+                    }
+
+                    itemHolder.click(R.id.open_file) {
+                        dslFHelper {
+                            fileSelector {
+
                             }
                         }
                     }
