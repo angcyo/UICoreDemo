ae3fc25bb2af8a5e9c639d8d413eb5b960d20f86
angcyo <angcyo@126.com>
2020-06-12 19:25:05 +0800

    * fix amap demo

--- Demo/src/main/java/com/angcyo/uicore/demo/AMapDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/AMapDemo.kt
@@ -1,7 +1,11 @@
 package com.angcyo.uicore.demo
 
 import android.os.Bundle
+import com.amap.api.maps.model.BitmapDescriptorFactory
+import com.amap.api.maps.model.LatLng
+import com.angcyo.amap3d.addNavigateArrow
 import com.angcyo.amap3d.core.RTextureMapView
+import com.angcyo.amap3d.onMapLoadedListener
 import com.angcyo.uicore.base.AppTitleFragment
 
 /**
@@ -18,7 +22,18 @@ class AMapDemo : AppTitleFragment() {
 
     override fun initBaseView(savedInstanceState: Bundle?) {
         super.initBaseView(savedInstanceState)
-        _vh.v<RTextureMapView>(R.id.map_view)?.bindLifecycle(this, savedInstanceState)
+        _vh.v<RTextureMapView>(R.id.map_view)?.apply {
+            dslAMap.locationIcon = BitmapDescriptorFactory.fromResource(R.drawable.map_gps_point)
+
+            bindLifecycle(this@AMapDemo, savedInstanceState)
+
+            map.onMapLoadedListener {
+                map.addNavigateArrow {
+                    add(LatLng(map.myLocation.latitude, map.myLocation.longitude))
+                    width(100f)
+                }
+            }
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
