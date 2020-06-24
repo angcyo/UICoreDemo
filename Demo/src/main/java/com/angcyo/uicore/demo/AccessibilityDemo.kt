a13c585c1119c44bbe80e38a339c68463d348124
angcyo <angcyo@126.com>
2020-06-24 17:57:40 +0800

    + accessibility service demo

--- Demo/src/main/java/com/angcyo/uicore/demo/AccessibilityDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/AccessibilityDemo.kt
@@ -1,9 +1,12 @@
 package com.angcyo.uicore.demo
 
 import android.os.Bundle
-import com.angcyo.core.component.accessibility.AccessibilityHelper
+import com.angcyo.core.component.accessibility.AccessibilityPermission
 import com.angcyo.dsladapter.renderItem
+import com.angcyo.library.ex.copy
+import com.angcyo.library.ex.openApp
 import com.angcyo.uicore.base.AppDslFragment
+import com.angcyo.widget.base.string
 
 /**
  *
@@ -20,10 +23,31 @@ class AccessibilityDemo : AppDslFragment() {
                 itemLayoutId = R.layout.item_accessibility_demo
 
                 itemBindOverride = { itemHolder, _, _, _ ->
-                    itemHolder.cb(R.id.cb_enable)?.isChecked = false
+                    itemHolder.cb(R.id.overlays_enable)?.isChecked =
+                        AccessibilityPermission.haveDrawOverlays(fContext())
+                    itemHolder.cb(R.id.accessibility_enable)?.isChecked =
+                        AccessibilityPermission.haveAccessibilityService(fContext())
 
-                    itemHolder.click(R.id.set_button) {
-                        AccessibilityHelper.openAccessibilityActivity(fContext())
+
+                    itemHolder.click(R.id.overlays_button) {
+                        AccessibilityPermission.openOverlaysActivity(fContext())
+                    }
+                    itemHolder.click(R.id.accessibility_button) {
+                        AccessibilityPermission.openAccessibilityActivity(fContext())
+                    }
+                    itemHolder.click(R.id.check_button) {
+                        itemHolder.tv(R.id.lib_text_view)?.text =
+                            "${AccessibilityPermission.check(fContext())}"
+                    }
+
+
+                    itemHolder.click(R.id.dy_button) {
+                        itemHolder.tv(R.id.dy_edit)?.string()?.copy()
+                        fContext().openApp("com.ss.android.ugc.aweme")
+                    }
+                    itemHolder.click(R.id.ks_button) {
+                        itemHolder.tv(R.id.ks_edit)?.string()?.copy()
+                        fContext().openApp("com.smile.gifmaker")
                     }
                 }
             }
