118a1649cf11541bff803ac9c41ed141a02a0dc3
angcyo <angcyo@126.com>
2020-09-23 22:19:53 +0800

    + skeleton view demo

--- Demo/src/main/java/com/angcyo/uicore/demo/SkeletonViewDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/SkeletonViewDemo.kt
@@ -1,8 +1,10 @@
 package com.angcyo.uicore.demo
 
 import android.os.Bundle
+import com.angcyo.drawable.skeleton.SkeletonDrawable
 import com.angcyo.dsladapter.dslItem
 import com.angcyo.uicore.base.AppDslFragment
+import com.angcyo.widget.SkeletonView
 
 /**
  *
@@ -12,11 +14,27 @@ import com.angcyo.uicore.base.AppDslFragment
  * Copyright (c) 2020 ShenZhen Wayto Ltd. All rights reserved.
  */
 class SkeletonViewDemo : AppDslFragment() {
+
+    companion object {
+        var count = 0
+    }
+
     override fun initBaseView(savedInstanceState: Bundle?) {
         super.initBaseView(savedInstanceState)
 
         renderDslAdapter {
-            dslItem(R.layout.demo_skeleton_layout)
+            dslItem(R.layout.demo_skeleton_layout) {
+                itemBindOverride = { itemHolder, itemPosition, adapterItem, payloads ->
+                    itemHolder.v<SkeletonView>(R.id.skeleton_view)
+                        ?.firstDrawable<SkeletonDrawable>()?.apply {
+                            if ((count++ % 2) == 0) {
+                                loadNormalSkeleton()
+                            } else {
+                                loadNormalSkeleton2()
+                            }
+                        }
+                }
+            }
         }
     }
 }
