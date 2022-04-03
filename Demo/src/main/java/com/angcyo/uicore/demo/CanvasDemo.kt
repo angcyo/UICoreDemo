2e22034c0be1e30bb216c3a629f3c2a3aa1c3b43
angcyo <angcyo@126.com>
2022-04-03 12:33:11 +0800

    + canvas text demo

--- Demo/src/main/java/com/angcyo/uicore/demo/CanvasDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/CanvasDemo.kt
@@ -2,6 +2,8 @@ package com.angcyo.uicore.demo
 
 import android.os.Bundle
 import com.angcyo.canvas.CanvasView
+import com.angcyo.canvas.core.component.items.TextItem
+import com.angcyo.canvas.core.renderer.items.TextItemRenderer
 import com.angcyo.dsladapter.bindItem
 import com.angcyo.uicore.base.AppDslFragment
 
@@ -38,6 +40,17 @@ class CanvasDemo : AppDslFragment() {
                 }
                 //缩小
                 itemHolder.click(R.id.scale_out_button) {
+                    canvasView?.canvasViewBox?.scaleBy(.8f, .8f)
+                }
+                //add
+                itemHolder.click(R.id.add_text) {
+                    canvasView?.apply {
+                        addCentreItemRenderer(TextItemRenderer(TextItem().apply {
+                            text = "angcyo"
+                        }, canvasViewBox))
+                    }
+                }
+                itemHolder.click(R.id.add_svg) {
                     canvasView?.canvasViewBox?.scaleBy(.8f, .8f)
                 }
             }
