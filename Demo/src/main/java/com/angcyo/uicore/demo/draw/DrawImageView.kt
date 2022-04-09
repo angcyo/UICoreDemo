f9e63142ba5e7ad7f63b301550aafda3cab2e6bf
angcyo <angcyo@126.com>
2022-04-09 11:50:41 +0800

    * update matrix draw

--- Demo/src/main/java/com/angcyo/uicore/demo/draw/DrawImageView.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/draw/DrawImageView.kt
@@ -1,14 +1,14 @@
 package com.angcyo.uicore.demo.draw
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Matrix
+import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.graphics.withMatrix
 import com.angcyo.canvas.utils.createPaint
+import com.angcyo.canvas.utils.mapPoint
 import com.angcyo.library.ex.dp
 import com.angcyo.library.ex.setBounds
 import com.angcyo.uicore.demo.R
@@ -32,10 +32,25 @@ class DrawImageView(context: Context, attributeSet: AttributeSet? = null) :
         strokeWidth = 2 * dp
     }
 
+    val circlePaint = createPaint(Color.RED, Paint.Style.FILL)
+    val circleSize = 2 * dp
+    val circleRect: RectF = RectF()
+
+    val rect: RectF = RectF()
+    val drawRect: RectF = RectF()
+
     init {
         drawable = resources.getDrawable(R.drawable.face, getContext().theme)
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        rect.set(0f, 0f, w.toFloat(), h.toFloat())
+        circleRect.set(rect)
+        circleRect.inset(-circleSize * 2, -circleSize * 2)
+    }
+
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
@@ -46,6 +61,33 @@ class DrawImageView(context: Context, attributeSet: AttributeSet? = null) :
             canvas.drawRect(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat(), paint)
         }
 
+        drawableMatrix.mapRect(drawRect, rect)
+        canvas.drawRect(drawRect, paint)
+
+        //circleMatrix.set(drawableMatrix)
+
+        val center = PointF(circleRect.centerX(), circleRect.centerY())
+        val _center = drawableMatrix.mapPoint(center)
+
+        val lt = PointF(circleRect.left, circleRect.top)
+        val _lt = drawableMatrix.mapPoint(lt)
+
+        val rt = PointF(circleRect.right, circleRect.top)
+        val _rt = drawableMatrix.mapPoint(rt)
+
+        val rb = PointF(circleRect.right, circleRect.bottom)
+        val _rb = drawableMatrix.mapPoint(rb)
+
+        val lb = PointF(circleRect.left, circleRect.bottom)
+        val _lb = drawableMatrix.mapPoint(lb)
+
+        canvas.drawCircle(_center.x, _center.y, circleSize, circlePaint)
+        canvas.drawCircle(_lt.x, _lt.y, circleSize, circlePaint)
+        canvas.drawCircle(_lb.x, _lb.y, circleSize, circlePaint)
+        canvas.drawCircle(_rt.x, _rt.y, circleSize, circlePaint)
+        canvas.drawCircle(_rb.x, _rb.y, circleSize, circlePaint)
+
+        //val c1 = CanvasTouchHandler.spacing()
     }
 
 }
