713580d58d56f145b867f6893f451c8169fd190a
angcyo <angcyo@126.com>
2022-05-23 15:22:53 +0800

    + compliance check

--- Demo/src/main/java/com/angcyo/uicore/SplashActivity.kt
+++ Demo/src/main/java/com/angcyo/uicore/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.angcyo.uicore
 import android.os.Bundle
 import com.angcyo.activity.BaseAppCompatActivity
 import com.angcyo.base.dslAHelper
+import com.angcyo.core.component.ComplianceCheck
 import com.angcyo.noAnim
 
 /**
@@ -12,6 +13,15 @@ import com.angcyo.noAnim
  * @date 2020/01/19
  */
 class SplashActivity : BaseAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ComplianceCheck.check {
+            //同意合规
+            ComplianceCheck.agree()
+        }
+    }
+
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
         dslAHelper {
