0f845f3b6abc2e06b13f540dacb2d6b857868b7e
angcyo <angcyo@126.com>
2022-06-14 18:08:50 +0800

    + color picker demo

--- Demo/src/main/java/com/angcyo/uicore/demo/LoadingDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/LoadingDemo.kt
@@ -1,13 +1,17 @@
 package com.angcyo.uicore.demo
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.angcyo.drawable.loading.TGSolidLoadingDrawable
 import com.angcyo.drawable.loading.TGStrokeLoadingDrawable
 import com.angcyo.dsladapter.bindItem
+import com.angcyo.github.dialog.colorPickerDialog
 import com.angcyo.library.ex.anim
 import com.angcyo.library.ex.dp
 import com.angcyo.library.ex.setBgDrawable
 import com.angcyo.library.ex.toColorInt
+import com.angcyo.uicore.MainFragment
 import com.angcyo.uicore.base.AppDslFragment
 import com.angcyo.widget.progress.DslBlockSeekBar
 import com.angcyo.widget.progress.DslProgressBar
@@ -74,6 +78,25 @@ class LoadingDemo : AppDslFragment() {
                         onSeekChanged = { value, fraction, fromUser ->
                             itemHolder.v<DslProgressBar>(R.id.progress_bar)
                                 ?.setProgress(value, value)
+                        }
+                    }
+                }
+
+                //color
+                itemHolder.click(R.id.color_picker_button) {
+                    val seekBar = itemHolder.v<DslBlockSeekBar>(R.id.seek_bar)
+                    fContext().colorPickerDialog {
+                        initialColor =
+                            (seekBar?.progressBgDrawable as? GradientDrawable)?.color?.defaultColor
+                                ?: initialColor
+                        colorPickerAction = { dialog, color ->
+                            seekBar?.setBgGradientColors("$color")
+                            false
+                        }
+
+                        if (MainFragment.CLICK_COUNT++ % 2 == 0) {
+                            paletteDrawable =
+                                ContextCompat.getDrawable(fContext(), R.drawable.palettebar)
                         }
                     }
                 }
