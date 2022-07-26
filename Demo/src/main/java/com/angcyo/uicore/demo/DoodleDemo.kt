2f67f8128412f4dbbfb98b0461e648874ddfc204
angcyo <angcyo@126.com>
2022-07-26 20:57:56 +0800

    + demo

--- Demo/src/main/java/com/angcyo/uicore/demo/DoodleDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/DoodleDemo.kt
@@ -2,6 +2,8 @@ package com.angcyo.uicore.demo
 
 import android.os.Bundle
 import com.angcyo.doodle.DoodleView
+import com.angcyo.doodle.brush.PenBrush
+import com.angcyo.doodle.brush.ZenBrush
 import com.angcyo.dsladapter.bindItem
 import com.angcyo.uicore.base.AppDslFragment
 
@@ -21,12 +23,22 @@ class DoodleDemo : AppDslFragment() {
             bindItem(R.layout.doodle_layout) { itemHolder, itemPosition, adapterItem, payloads ->
                 val doodleView = itemHolder.v<DoodleView>(R.id.doodle_view)
 
+                //
                 itemHolder.click(R.id.undo_button) {
                     doodleView?.doodleDelegate?.undoManager?.undo()
                 }
 
                 itemHolder.click(R.id.redo_button) {
                     doodleView?.doodleDelegate?.undoManager?.redo()
+                }
+
+                //
+                itemHolder.click(R.id.pen_button) {
+                    doodleView?.doodleDelegate?.doodleTouchManager?.updateTouchRecognize(PenBrush())
+                }
+
+                itemHolder.click(R.id.zen_button) {
+                    doodleView?.doodleDelegate?.doodleTouchManager?.updateTouchRecognize(ZenBrush())
                 }
 
             }
