ca3a062c138e4524cd92104f9ccea2773c78284f
angcyo <angcyo@126.com>
2022-08-04 12:02:49 +0800

    + http / media demo

--- Demo/src/main/java/com/angcyo/uicore/demo/HttpDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/HttpDemo.kt
@@ -1,12 +1,18 @@
 package com.angcyo.uicore.demo
 
+import android.graphics.Color
 import android.os.Bundle
 import com.angcyo.dsladapter.bindItem
 import com.angcyo.http.base.jsonObject
 import com.angcyo.http.post
 import com.angcyo.http.rx.ToastObserver
 import com.angcyo.http.rx.observer
+import com.angcyo.item.DslGridMediaItem
+import com.angcyo.item.style.addGridMedia
+import com.angcyo.item.style.gridMediaSpanCount
 import com.angcyo.library.L
+import com.angcyo.library.ex._colorDrawable
+import com.angcyo.pager.dslitem.DslNineMediaItem
 import com.angcyo.uicore.base.AppDslFragment
 import com.angcyo.widget.base.setInputText
 import com.angcyo.widget.base.string
@@ -54,6 +60,28 @@ class HttpDemo : AppDslFragment() {
                         })
                     }
                 }
+            }
+
+            //media
+            DslGridMediaItem()() {
+                itemBackgroundDrawable = _colorDrawable(Color.RED)
+                addGridMedia("http://laserpecker.oss-cn-shenzhen.aliyuncs.com/app/images/47f9c14b-813b-d697-cadb-f0f0d99d46af.png")
+            }
+            DslNineMediaItem()() {
+                itemBackgroundDrawable = _colorDrawable(Color.BLUE)
+                addGridMedia("http://laserpecker.oss-cn-shenzhen.aliyuncs.com/app/images/bd7d94fc-ef75-a48c-fb2a-db3a8d3b567d.png")
+            }
+            DslNineMediaItem()() {
+                itemBackgroundDrawable = _colorDrawable(Color.RED)
+                addGridMedia("http://laserpecker.oss-cn-shenzhen.aliyuncs.com/app/images/bd7d94fc-ef75-a48c-fb2a-db3a8d3b567d.png")
+                addGridMedia("http://laserpecker.oss-cn-shenzhen.aliyuncs.com/app/images/d446f1f3-9227-fe64-543c-93e3dad669fe.png")
+            }
+            DslNineMediaItem()() {
+                itemBackgroundDrawable = _colorDrawable(Color.RED)
+                addGridMedia("http://laserpecker.oss-cn-shenzhen.aliyuncs.com/app/images/bd7d94fc-ef75-a48c-fb2a-db3a8d3b567d.png")
+                addGridMedia("http://laserpecker.oss-cn-shenzhen.aliyuncs.com/app/images/47f9c14b-813b-d697-cadb-f0f0d99d46af.png")
+                addGridMedia("http://laserpecker.oss-cn-shenzhen.aliyuncs.com/app/images/4b7a140c-7ff7-550b-bab6-55967dcdcdde.png")
+                gridMediaSpanCount = 3
             }
         }
     }
