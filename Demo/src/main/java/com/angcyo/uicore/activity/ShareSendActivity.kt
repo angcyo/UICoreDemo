21773ef5b4a606c792ddd215a30661095a0d3730
angcyo <angcyo@126.com>
2023-04-09 14:11:30 +0800

    + demo

---
+++ Demo/src/main/java/com/angcyo/uicore/activity/ShareSendActivity.kt
@@ -0,0 +1,30 @@
+package com.angcyo.uicore.activity
+
+import android.os.Bundle
+import com.angcyo.base.dslFHelper
+import com.angcyo.core.activity.BaseCoreAppCompatActivity
+import com.angcyo.core.component.ShareSendFragment
+import com.angcyo.library.component.ROpenFileHelper
+import com.angcyo.putParcelableList
+
+/**
+ * 分享过来用来发送文件到指定服务器
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2023/04/09
+ * Copyright (c) 2020 angcyo. All rights reserved.
+ */
+class ShareSendActivity : BaseCoreAppCompatActivity() {
+    override fun onCreateAfter(savedInstanceState: Bundle?) {
+        super.onCreateAfter(savedInstanceState)
+
+        dslFHelper {
+            removeAll()
+            show(ShareSendFragment::class) {
+                ROpenFileHelper.parseIntentUri(intent)?.let {
+                    putParcelableList(it, ShareSendFragment.KEY_URI_LIST)
+                }
+            }
+        }
+    }
+}
