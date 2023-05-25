977d9c955d5f956fe01746315945771d717c0b8f
angcyo <angcyo@126.com>
2023-05-25 19:40:04 +0800

    * fix demo

--- Demo/src/main/java/com/angcyo/uicore/demo/Canvas2Demo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/Canvas2Demo.kt
@@ -1,5 +1,6 @@
 package com.angcyo.uicore.demo
 
+import android.graphics.Matrix
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.RectF
@@ -89,6 +90,7 @@ import com.angcyo.objectbox.laser.pecker.entity.TransferConfigEntity
 import com.angcyo.toSVGContent
 import com.angcyo.uicore.base.AppDslFragment
 import com.angcyo.uicore.getRandomText
+import com.angcyo.vector.toPath
 import com.angcyo.widget.DslViewHolder
 import com.angcyo.widget.span.span
 
@@ -632,14 +634,35 @@ class Canvas2Demo : AppDslFragment(), IEngraveRenderFragment {
             toastQQ("test")*/
             //EngraveTransitionHelper.saveTaskAerialView("e2bebf35ff624b9bba2f9bce86633524")
 
+            "M12.981 6.677c.006.122.008.243.008.366a7.991 7.991 0 0 1-8.046 8.047 8.01 8.01 0 0 1-4.335-1.271 5.805 5.805 0 0 0 .675.04 5.68 5.68 0 0 0 3.513-1.21 2.832 2.832 0 0 1-2.642-1.965 2.813 2.813 0 0 0 .531.051 2.865 2.865 0 0 0 .746-.1 2.831 2.831 0 0 1-2.269-2.772v-.037a2.805 2.805 0 0 0 1.281.354 2.828 2.828 0 0 1-.874-3.775 8.024 8.024 0 0 0 5.828 2.954 2.83 2.83 0 0 1 2.755-3.472 2.826 2.826 0 0 1 2.064.893 5.654 5.654 0 0 0 1.796-.686 2.835 2.835 0 0 1-1.244 1.564 5.642 5.642 0 0 0 1.624-.445 5.735 5.735 0 0 1-1.41 1.464z".toPath()
+                .apply {
+                    val matrix = Matrix()
+                    matrix.setScale(10f, 10f)
+                    transform(matrix)
+                    testPath(this)
+                }
+
+            /*val o = PointF(0f, 0f)
+            val p1 = PointF(45f, -45f)
+            val p2 = PointF(-45f, -45f)
+            val p3 = PointF(-45f, 45f)
+            val p4 = PointF(45f, 45f)
+            L.i(buildString {
+                appendLine()
+                appendLine("P1: $p1 ${VectorHelper.angle(o, p1)}")
+                appendLine("P2: $p2 ${VectorHelper.angle(o, p2)}")
+                appendLine("P3: $p3 ${VectorHelper.angle(o, p3)}")
+                appendLine("P4: $p4 ${VectorHelper.angle(o, p4)}")
+            })
+
             Path().apply {
                 //addCircle(100f, 100f, 100f, Path.Direction.CW)
                 addRoundRect(0f, 0f, 200f, 200f, 40f, 40f, Path.Direction.CW)
 
-                /*transform(Matrix().apply {
+                *//*transform(Matrix().apply {
                     setRotate(30f, 100f, 100f)
                     postSkew(0.3f, 0f)
-                })*/
+                })*//*
 
                 testPath(this)
             }
@@ -647,7 +670,7 @@ class Canvas2Demo : AppDslFragment(), IEngraveRenderFragment {
             Path().apply {
                 addCircle(100f, 100f, 100f, Path.Direction.CW)
                 testPath(this)
-            }
+            }*/
         }
     }
 
