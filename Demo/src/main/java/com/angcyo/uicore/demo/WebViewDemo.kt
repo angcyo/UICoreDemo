187c42b694c4bcb99c5492e6012263b8df8bbfd1
angcyo <angcyo@126.com>
2023-06-08 18:39:53 +0800

    * fix demo

--- Demo/src/main/java/com/angcyo/uicore/demo/WebViewDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/WebViewDemo.kt
@@ -1,10 +1,13 @@
 package com.angcyo.uicore.demo
 
-import android.view.ViewGroup
+import android.webkit.JavascriptInterface
+import androidx.annotation.Keep
+import com.angcyo.library.L
 import com.angcyo.library.ex.toUri
 import com.angcyo.library.toastQQ
 import com.angcyo.library.utils.UrlParse
 import com.angcyo.web.WebFragment
+import com.angcyo.web.core.DslWebView
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -14,11 +17,13 @@ class WebViewDemo : WebFragment() {
 
     init {
         webConfig.uri = "http://192.168.31.171:65050/?l=zh-CN".toUri()
+//        webConfig.uri = "https://www.baidu.com/".toUri()
     }
 
-    override fun attachWebView(url: String?, data: String?, parent: ViewGroup?) {
-        super.attachWebView(url, data, parent)
-        webView?.shouldOverrideUrlLoadAction = { webView, url ->
+    override fun onInitWebView(webView: DslWebView) {
+        webView.addJavascriptInterface(Logger(), "logger")
+
+        webView.shouldOverrideUrlLoadAction = { webView, url ->
             //hingin://laserpecker.com?t=ad8cec416fa84a13b089c44d661e3c56
             val url = url?.lowercase()
             if (url?.startsWith("hingin://laserpecker.com") == true) {
@@ -31,5 +36,18 @@ class WebViewDemo : WebFragment() {
             }
         }
     }
+}
 
+@Keep
+class Logger {
+
+    @JavascriptInterface
+    fun info(msg: Array<String?>?) {
+        L.i(msg)
+    }
+
+    @JavascriptInterface
+    fun error(msg: Array<String?>?) {
+        L.e(msg)
+    }
 }
