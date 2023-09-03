4818d253845506a5238f9430b0c24b867fed6287
angcyo <angcyo@126.com>
2023-09-03 09:30:47 +0800

    + demo

--- Demo/src/main/java/com/angcyo/uicore/demo/JxlDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/JxlDemo.kt
@@ -8,8 +8,10 @@ import com.angcyo.core.component.fileSelector
 import com.angcyo.dsladapter.bindItem
 import com.angcyo.jxl.Jxl
 import com.angcyo.library.component.parser.parseDateTemplate
+import com.angcyo.library.component.parser.parseNumberTemplate
 import com.angcyo.library.ex._color
 import com.angcyo.library.ex.decode
+import com.angcyo.library.ex.nowTime
 import com.angcyo.library.ex.toStr
 import com.angcyo.library.libCacheFile
 import com.angcyo.library.libFolderPath
@@ -87,6 +89,27 @@ class JxlDemo : AppDslFragment() {
                 itemHolder.click(R.id.parse_date_button) {
                     val text = itemHolder.tv(R.id.edit_text)?.string(false)
                     itemHolder.tv(R.id.result_text_view)?.text = text?.parseDateTemplate()
+                }
+
+                itemHolder.click(R.id.parse_number_button) {
+                    val template = itemHolder.tv(R.id.edit_number_template)?.string(false)
+                    val text = itemHolder.tv(R.id.edit_number)?.string(true)
+                    itemHolder.tv(R.id.result_text_view)?.text = span {
+                        val number1 = text ?: nowTime().toString()
+                        append(number1 + "->" + template?.parseNumberTemplate(number1))
+                        append("\n")
+                        val number2 = nowTime().toString()
+                        append(number2 + "->" + template?.parseNumberTemplate(number2))
+                        append("\n")
+                        val number3 = "${nowTime()}.${nowTime()}"
+                        append(number3 + "->" + template?.parseNumberTemplate(number3))
+                        append("\n")
+                        val number4 = "${nowTime()}."
+                        append(number4 + "->" + template?.parseNumberTemplate(number4))
+                        append("\n")
+                        val number5 = ".${nowTime()}"
+                        append(number5 + "->" + template?.parseNumberTemplate(number5))
+                    }
                 }
             }
         }
