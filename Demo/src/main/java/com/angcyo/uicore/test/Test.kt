ba1858c7bb52a874006057b0424d84897e59545c
angcyo <angcyo@126.com>
2023-09-04 15:46:39 +0800

    * fix test

--- Demo/src/main/java/com/angcyo/uicore/test/Test.kt
+++ Demo/src/main/java/com/angcyo/uicore/test/Test.kt
@@ -1,8 +1,17 @@
 package com.angcyo.uicore.test
 
+import android.graphics.PointF
 import com.angcyo.library.L
 import com.angcyo.library.annotation.TestPoint
 import com.angcyo.library.component.parser.parseDateTemplate
+import com.angcyo.library.component.pool.acquireTempMatrix
+import com.angcyo.library.component.pool.acquireTempPointF
+import com.angcyo.library.component.pool.release
+import com.angcyo.library.ex.angle
+import com.angcyo.library.ex.mapPoint
+import com.angcyo.library.ex.rotate
+import kotlin.math.atan
+import kotlin.math.tan
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -57,12 +66,66 @@ object Test {
         //V8Engine.test()
         //QuickJSEngine.test()
 
-        testTemplateParse()
+        //testPoint()
+        //testTemplateParse()
     }
 
     private fun testTemplateParse() {
-        val result = "[[YYYYescape] [[angcyo]] YYYY-MM-DDTHH:mm:ss:SS Z[Z] d dd ZZ A a".parseDateTemplate()
+        val result =
+            "[[YYYYescape] [[angcyo]] YYYY-MM-DDTHH:mm:ss:SS Z[Z] d dd ZZ A a".parseDateTemplate()
         L.d(result)
     }
 
+    private fun testPoint() {
+        val p1 = PointF(10f, 10f)
+        val p2 = PointF(20f, 20f)
+        //val p2 = PointF(20f, 20f)
+
+        //计算2个点之间的角度
+        val a1 = p1.angle(p2)
+        //val a2 = p2.angle(p1)
+
+        val width = 0.1f
+        val stepHeight = 0.04f
+
+        val rotateAngle = 90f - a1
+        val reverseMatrix = acquireTempMatrix()
+        reverseMatrix.setRotate(-rotateAngle, p1.x, p1.y)
+        p2.rotate(rotateAngle, p1.x, p1.y)
+
+        val startX = p1.x - width / 2
+        val startY = p1.y
+        val endX = p2.x + width / 2
+        val endY = p2.y
+
+        val angle = Math.toDegrees(atan(stepHeight / width).toDouble())
+
+        //使用指定角度的线填充矩形
+        var x = startX
+        var y = startY
+        val builder = StringBuilder()
+        val tempPointF = acquireTempPointF()
+        while (y <= endY) {
+            tempPointF.set(x, y)
+            reverseMatrix.mapPoint(tempPointF)
+
+            if (x == startX && y == startY) {
+                //第一个点
+                builder.appendLine("G0X${tempPointF.x}Y${tempPointF.y}")
+                x = endX
+                continue
+            }
+            builder.appendLine("G1X${tempPointF.x}Y${tempPointF.y}")
+
+            if (x >= endX) {
+                x = startX
+            } else {
+                x = endX
+            }
+            y += tan(Math.toRadians(angle)).toFloat() * width
+        }
+        tempPointF.release()
+        reverseMatrix.release()
+        L.i(builder)
+    }
 }
