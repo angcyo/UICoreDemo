f137263c02f4bad2c797136f5f7ee982f7860d40
angcyo <angcyo@126.com>
2025-03-26 15:26:54 +0800

    + demo

--- Demo/src/main/java/com/angcyo/uicore/demo/OpenGLDemo.kt
+++ Demo/src/main/java/com/angcyo/uicore/demo/OpenGLDemo.kt
@@ -1,13 +1,17 @@
 package com.angcyo.uicore.demo
 
 import android.graphics.Color
+import android.graphics.PointF
 import android.os.Bundle
 import com.angcyo.library.ex.stackOf
 import com.angcyo.opengl.core.BaseOpenGLRenderer
 import com.angcyo.opengl.core.IOpenGLSurface
 import com.angcyo.opengl.core.Vector3
+import com.angcyo.opengl.primitives.OpenGLGCodeLine
+import com.angcyo.opengl.primitives.OpenGLGCodeLineData
 import com.angcyo.opengl.primitives.OpenGLLine
 import com.angcyo.uicore.base.AppTitleFragment
+import com.angcyo.widget.seek
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -19,25 +23,61 @@ class OpenGLDemo : AppTitleFragment() {
         contentLayoutId = R.layout.fragment_opengl
     }
 
+    /**线条*/
+    var openglGCodeLine: OpenGLGCodeLine? = null
+
     override fun initBaseView(savedInstanceState: Bundle?) {
         super.initBaseView(savedInstanceState)
+        val seekBar = _vh.seek(R.id.seek_bar) { value, fraction, fromUser ->
+            openglGCodeLine?.setRendererProgress(fraction)
+        }
+
         val openGlSurface: IOpenGLSurface? = _vh.view(R.id.opengl_view) as IOpenGLSurface?
         openGlSurface?.setSurfaceRenderer(object : BaseOpenGLRenderer(fContext()) {
             override fun initScene() {
                 //getCurrentScene().addChild(TestOpenGLObject())
-                val points = stackOf(
-                    Vector3(-0.5f, 0.5f, 0f),
-                    Vector3(0f, 0.5f, 0f),
-                    Vector3(0f, -0.5f, 0f),
-                    Vector3(0.5f, -0.5f, 0f),
-                )
                 val colors = intArrayOf(
                     Color.RED,
                     Color.GREEN,
                     Color.BLUE,
                     Color.YELLOW,
                 )
-                getCurrentScene().addChild(OpenGLLine(points, colors))
+                getCurrentScene().addChild(
+                    OpenGLLine(
+                        stackOf(
+                            Vector3(-0.5f, 0.5f, 0f),
+                            Vector3(0f, 0.5f, 0f),
+                            Vector3(0f, -0.5f, 0f),
+                            Vector3(0.5f, -0.5f, 0f),
+                        ), colors
+                    )
+                )
+                getCurrentScene().addChild(
+                    OpenGLGCodeLine(
+                        stackOf(
+                            OpenGLGCodeLineData(
+                                startPoint = PointF(1f, 1f),
+                                endPoint = PointF(2f, 1f),
+                                color = Color.RED,
+                            ),
+                            OpenGLGCodeLineData(
+                                startPoint = PointF(2f, 1f),
+                                endPoint = PointF(2f, 2f),
+                                color = Color.GREEN,
+                            ),
+                            OpenGLGCodeLineData(
+                                startPoint = PointF(2f, 2f),
+                                endPoint = PointF(3f, 2f),
+                                color = Color.BLUE,
+                            ),
+                        )
+                    ).apply {
+                        //getModelMatrix()
+                        openglGCodeLine = this
+                    }
+                )
+
+                getCurrentScene().scaleSceneBy(0.25f, 0.25f)
             }
         })
     }
